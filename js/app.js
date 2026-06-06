49a734d75475c9efcccfbf12335ac4e2c537108b
Vk <Sullorak@gmail.com>
2026-06-06 16:30:19 +0200

    inital setup

---
+++ js/app.js
@@ -0,0 +1,2 @@
+import { changec } from "./functions.js"
+document.getElementById("button").addEventListener("click",changec)
---
+++ js/functions.js
@@ -0,0 +1,4 @@
+export function changec(){
+const bot=document.getElementById("button");
+bot.innerHTML="bbu"
+}
